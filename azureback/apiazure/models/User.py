a19446ffdfe31e60e42a24ea44db9eeadfbe8909
Guilherme Almeida Lopes <guiguigui098@gmail.com>
2024-11-03 00:14:57 -0400

    feat: User model

---
+++ azureback/apiazure/models/User.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class User(models.Model):
+    name=models.CharField(max_length=500)
+    email=models.EmailField(max_length=250,primary_key=True)
+    password=models.CharField(max_length=100)
+    imagefield=models.ImageField()
+    
