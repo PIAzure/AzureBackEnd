faee6169afe21fbb176903fe094c10a5028f1cb9
Guilherme Almeida Lopes <guiguigui098@gmail.com>
2024-12-12 23:55:06 -0300

    fix: bug error get all event of organizator

--- azureback/apiazure/Urls/EventsUrl.py
+++ azureback/apiazure/Urls/EventsUrl.py
@@ -2,7 +2,7 @@ from apiazure.views.Eventsview import EventDetailListPost, EventAdminDetail, Eve
 from rest_framework.urls import path
 urlpatterns = [
     path('',EventDetailListPost.as_view(),name="criar usuário"),
-    path('<str:email>/',EventDetailListGet.as_view(),name="pegar todos os users"),
+    path('event/organizator/<str:email>/',EventDetailListGet.as_view(),name="pegar todos os users"),
     path("event/<int:primary_key>/",EventDetail.as_view(),name="pegar,atualizar e deletar"),
     path("admin/all/",EventAdminDetail.as_view(),name="pegar todos os eventos cadastrados")
 ]
